3dafcad7e56999531e3d2252c077c93652546f08
Scot Hampel <scot@scothampel.com>
2021-10-13 15:45:06 -0500

    Transfer comp layout with actual data refs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import '../styles/App.css';
 import Transfer from './Transfer';
 
 function App() {
+  const [transfers, setTransfers] = useState({current_transfers: {} })
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/transfers')
+      .then(res => res.json())
+      .then(data => {
+        setTransfers(data)
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <div className='container mt-3'>
-      <Transfer name='Test Movie something long.wfwf.fgwfwwadawdwadwadadwdwadawddawdwada' progress='20' />
+      {
+        !loading &&
+        Object.entries(transfers.current_transfers).map((v, i) => {
+          return <Transfer key={i} name={v[0]} files={v[1].file_list} progress={v[1].file_prog} complete={false} />
+        })
+      }
     </div>
+    
   )
 }
 
--- client/src/components/Transfer.js
+++ client/src/components/Transfer.js
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react'
 import '../styles/Transfer.css'
 
-export default function Transfer({ name, files, progress }) {
+export default function Transfer({ name, files, progress, complete }) {
+  const [currentFile, setCurrentFile] = useState('')
+  const [currentProgress, setCurrentProgress] = useState('')
+
+  useEffect(() => {
+    setCurrentFile(files[files.length - 1])
+    setCurrentProgress(progress[progress.length - 1].match(/\S+/g))
+  }, [files, progress])
+
   return (
-    <div className='Transfer' >
+    <div className='Transfer mb-3' >
       <div className='card'>
         <div className='card-body'>
           <h5 className='card-title w-75 pb-1 overflow-ellipsis'>{name}</h5>
-          <p className='card-text mb-0'>Test text</p>
-              <div className='progress'>
-                <div className='progress-bar progress-bar-striped progress-bar-animated' role='progressbar' style={{ width: `${progress}%` }}>{`${progress}%`}</div>
-                <div className='progress-bar bg-secondary' role='progressbar' style={{ width: `${100 - parseInt(progress)}%` }}>0.00 MB/s</div>
-              </div>
+          <div className='d-flex justify-content-between'>
+            <p className='card-text mb-0'>{currentFile}</p>
+            <span>{currentProgress[3]}</span>
+          </div>
+          <div className='progress'>
+            <div className={`progress-bar progress-bar-striped progress-bar-animated ${complete ? 'bg-success' : ''}`} role='progressbar' style={{ width: currentProgress[1] }}>
+              {currentProgress[1]} / {currentProgress[0]}
+            </div>
+            {!complete && <div className='progress-bar bg-secondary' role='progressbar' style={{ width: `${100 - parseInt(currentProgress[1])}%` }}>{currentProgress[2]}</div>}
+          </div>
         </div>
       </div>
     </div>
